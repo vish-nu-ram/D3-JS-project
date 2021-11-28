32c0fe74ba307ce6285c647a67aa88ed64cc1dc7
vish-nu-ram <vishnu.ram@hotmail.com>
2021-11-28 18:51:42 +0000

    v

--- udemy-d3-master/02/2.04/js/main.js
+++ udemy-d3-master/02/2.04/js/main.js
@@ -4,12 +4,13 @@
 *    2.4 - Adding SVGs with D3
 */
 
+const data = [250,12,34,76]
 const svg = d3.select("#chart-area").append("svg")
   .attr("width", 400)
   .attr("height", 400)
 
 svg.append("circle")
-  .attr("cx", 100)
+  .attr("cx", (data,i)=> ((i*10)+50))
   .attr("cy", 250)
   .attr("r", 70)
   .attr("fill", "red")
